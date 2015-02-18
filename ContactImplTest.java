18213c59806ff1829b20840b80342c998ffd8cfe
kieren millar <kieren_1@hotmail.com>
2015-02-18 15:20:20 +0000

    adding first test for contact

---
+++ ContactImplTest.java
@@ -0,0 +1,41 @@
+package cw4;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ContactImplTest {
+
+	@Before
+	public void setup(){
+		
+	}
+	
+	@After
+	public void takeDown(){
+		
+	}
+	
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNotes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNotes() {
+		fail("Not yet implemented");
+	}
+
+}
