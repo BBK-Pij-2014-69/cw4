b394d65e758b3019518639355de8b81b2d731925
kieren millar <kieren_1@hotmail.com>
2015-02-22 16:54:18 +0000

    initial commit of meeting impl and test

---
+++ MeetingImpl.java
@@ -0,0 +1,26 @@
+package cw4;
+
+import java.util.Calendar;
+import java.util.Set;
+
+public class MeetingImpl implements Meeting {
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ MeetingImplTest.java
@@ -0,0 +1,34 @@
+package cw4;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MeetingImplTest {
+
+	@Before
+	public void setUp() {
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetContacts() {
+		fail("Not yet implemented");
+	}
+
+}
