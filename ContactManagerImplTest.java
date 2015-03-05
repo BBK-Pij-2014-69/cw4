a6cd9cd49d20b6b3bbf306d56f7f95bdd0cebbc7
kieren millar <kieren_1@hotmail.com>
2015-03-05 13:49:22 +0000

    first commit of contact manager test

---
+++ ContactManagerImplTest.java
@@ -0,0 +1,84 @@
+package cw4;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ContactManagerImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddFutureMeeting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPastMeeting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFutureMeeting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMeeting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFutureMeetingListContact() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFutureMeetingListCalendar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPastMeetingList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNewPastMeeting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMeetingNotes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNewContact() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetContactsIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetContactsString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFlush() {
+		fail("Not yet implemented");
+	}
+
+}
