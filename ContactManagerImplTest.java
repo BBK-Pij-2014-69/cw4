5493ed5d57c6b13474eed9ed9b9ef236ecfbbf8c
kieren millar <kieren_1@hotmail.com>
2015-03-07 12:11:17 +0000

    wrote tests for addNewContact and getContact(String)

--- ContactManagerImplTest.java
+++ ContactManagerImplTest.java
@@ -16,25 +16,25 @@ import org.junit.rules.ExpectedException;
 public class ContactManagerImplTest {
 	ContactManager contactManager;
 	Set<Contact> contacts;
-	
 
 	@Before
 	public void setUp() {
 		contacts = new LinkedHashSet<Contact>(); // Used LinkedHashSet as order is maintained and so easier to test.
-		contacts.add(new ContactImpl(1, "Clark Kent"));
+		contacts.add(new ContactImpl(1, "Diana Prince"));
 		contactManager = new ContactmangerImpl();
 	}
 
 	@After
 	public void tearDown() {
-		
+	
 	}
 	
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
 
-	@Ignore @Test
+	@Test
 	public void testAddFutureMeeting() {
+		contactManager.addNewContact("Diana Prince", "I AM WONDER WOMAN");
 		assertEquals(1,contactManager.addFutureMeeting(contacts, new GregorianCalendar(2016, 00, 23, 12, 00)));
 	}
 	
@@ -93,9 +93,11 @@ public class ContactManagerImplTest {
 		fail("Not yet implemented");
 	}
 
-	@Ignore @Test
+	@Test
 	public void testAddNewContact() {
-		fail("Not yet implemented");
+		Contact contactToTest = new ContactImpl(1, "Bruce Wayne");
+		contactManager.addNewContact("Bruce Wayne", "I AM BATMAN");
+		assertTrue(contactManager.getContacts("Bruce Wayne").contains(contactToTest));
 	}
 
 	@Ignore @Test
@@ -103,9 +105,11 @@ public class ContactManagerImplTest {
 		fail("Not yet implemented");
 	}
 
-	@Ignore @Test
+	@Test
 	public void testGetContactsString() {
-		fail("Not yet implemented");
+		Contact contactToTest = new ContactImpl(1, "Clark Kent");
+		contactManager.addNewContact("Clark Kent", "I AM SUPERMAN");
+		assertTrue(contactManager.getContacts("Clark Kent").contains(contactToTest));
 	}
 
 	@Ignore @Test
